c21603fd2ef4f4b798d3fd579c5970184fb509ee
Alexander_Gallo <rodolfo.gallo@unl.edu.ec>
2025-07-21 18:19:19 -0500

    Archivos actuales

---
+++ QualitySystem/src/controller/utiles/Utilidades.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller.utiles;
+
+import java.io.*;
+
+public class Utilidades {
+    public String [][ ] listAll(String fileName) throws IOException {
+        int lineCount = 0;
+        BufferedReader br = new BufferedReader (new FileReader (fileName));
+        while (br.readLine() != null){
+            lineCount ++;
+        }
+        br.close();
+        
+        String [][] datos = new String [lineCount][];
+        br = new BufferedReader (new FileReader(fileName));
+        int index = 0;
+        String linea;
+        while ((linea = br.readLine())!= null){
+            datos[index++] = linea.split("\t");
+        }
+        br.close();
+        return datos;
+    }
+}
